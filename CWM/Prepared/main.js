e6a79edeac6c8c3133db4171a685900b5703a19e
Marou-Fer <72912738+Marou-Fer@users.noreply.github.com>
2024-07-16 14:36:13 +0200

    Update main.js

--- CWM/Prepared/main.js
+++ CWM/Prepared/main.js
@@ -1,7 +1,6 @@
 // Definie the Loading function of the third-party library echarts via $.getScript //
 
 
-//$.getScript('https://customwidgetcollectionprod.cfapps.eu10-004.hana.ondemand.com/fixReactUpgradeIssue.js')
 (function () {
 
 // Parse dimensions, measures, data from data binding results //
